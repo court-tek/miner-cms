3983fa382ad1cee910c996804be36b301d68dce7
court-tek <dishoughng@gmail.com>
2023-12-05 18:54:54 -0330

    added static data for landing preview section

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,22 @@ class LandingController < ApplicationController
     layout "landing"
 
     def index
+        @preview = [
+            {
+                :title => "dinner",
+                :description => "",
+                :image => ""
+            },
+            {
+                :title => "cocktails & wine",
+                :description => "",
+                :image => ""
+            },
+            {
+                :title => "children's",
+                :description => "",
+                :image => ""
+            },
+        ]
     end
 end
