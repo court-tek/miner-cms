1d0d860a5273bb71bdefe0ce2a15d0876748860c
court-tek <dishoughng@gmail.com>
2023-12-11 05:37:20 -0330

    fixed vendor js bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./src/header"
-import "./src/bootstrap.bundle"
-import "./src/jquery-3.3.1.min"
-import "./src/jquery.slimscroll"
 import "./src/test"
+import "./src/header"
